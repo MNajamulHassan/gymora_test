3d7e42e69f4db851d123f96c5bf755436fe596d4
M.Najam-ul-Hassan <M.Najamulhassan99@gmail.com>
2026-05-11 15:46:18 +0500

    Switch from SQLite to SQL Server
    Replace SQLite with SQL Server as the EF Core provider. Removed Microsoft.EntityFrameworkCore.Sqlite package reference from Gymora.csproj and updated Program.cs to call UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")) instead of UseSqlite("Data Source=gymora.db"). This makes the app use the configured DefaultConnection (SQL Server) for the application's database.

--- Gymora/Program.cs
+++ Gymora/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // 1. Database
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite("Data Source=gymora.db"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
