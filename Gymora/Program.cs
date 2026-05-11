3994a838e1670c76a7df4217e5e44d5f8d3f3737
M.Najam-ul-Hassan <M.Najamulhassan99@gmail.com>
2026-05-11 15:15:14 +0500

    Switch to Npgsql/PostgreSQL and UI tweaks
    Add Npgsql.EntityFrameworkCore.PostgreSQL package and update ApplicationDbContext to use Npgsql (UseNpgsql) with the corresponding using import. Adjust Plan view price display to use two decimals and show the PKR currency label separately for better styling. Add appsettings.Production.json with production logging level set to Warning.

--- Gymora/Program.cs
+++ Gymora/Program.cs
@@ -4,12 +4,13 @@ using Gymora.Services;
 using Gymora.Services.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // 1. Database
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
